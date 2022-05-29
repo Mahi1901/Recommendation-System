64b64f6496d09efa4c3604bc8e9867adec37dea4
Harsh Kushwaha <harshkushwaha2102@gmail.com>
2022-05-29 23:44:55 +0530

    master

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.register),
-    path('login/',views.login),
-    path('home/',views.home)
+    path('home/', views.home),
+    path('search/', views.search),
+    path('login/', views.loginhandler, name='login'),
+    path('', views.register, name='reg'),
+    path('movies/',views.Movies),
+    path('tv_shows/',views.TvShows)
+
+
 ]
